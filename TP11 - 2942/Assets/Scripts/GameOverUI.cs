3dd6301bbcd15d61145d889d0eda8de1108ce50a
Mauro <mauro.mendozacp@gmail.com>
2021-06-17 16:08:24 -0300

    game over added

---
+++ TP11 - 2942/Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameOverUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _resultText;
+    [SerializeField] private TMP_Text _scoreText;
+    [SerializeField] private TMP_Text _enemiesKilledText;
+
+    void Start()
+    {
+        _resultText.text = "YOU ???";
+        _scoreText.text = "Score: 0";
+        _enemiesKilledText.text = "Enemies Killed: 0";
+    }
+
+    public void BackToMenu()
+    {
+        //GameManager.Instance.ChangeScene("MainMenu");
+    }
+}
