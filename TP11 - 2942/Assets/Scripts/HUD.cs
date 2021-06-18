d7d62e773bfb252936d9adb42fce2b2c48d10f04
Mauro <mauro.mendozacp@gmail.com>
2021-06-18 01:30:27 -0300

    HUD and pause added

---
+++ TP11 - 2942/Assets/Scripts/HUD.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField] private Image energyBar;
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text bombsText;
+    [SerializeField] private TMP_Text enemiesKilled;
+    [SerializeField] private PauseUI pause;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SetPause();
+        }
+    }
+
+    public void SetPause()
+    {
+        pause.SetPause();
+    }
+
+    void UpdateEnergy()
+    {
+        energyBar.fillAmount = 1f / 100;
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "0" + "pts";
+    }
+
+    void UpdateBombs()
+    {
+        bombsText.text = "0";
+    }
+
+    void UpdateEnemiesKilled()
+    {
+        enemiesKilled.text = "0";
+    }
+}
---
+++ TP11 - 2942/Assets/Scripts/PauseUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseUI : MonoBehaviour
+{
+    public void SetPause()
+    {
+        Time.timeScale = 0.0f;
+        gameObject.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+        gameObject.SetActive(false);
+    }
+
+    public void BackToMenu()
+    {
+        //GameManager.Instance.ChangeScene("MainMenu");
+    }
+}
