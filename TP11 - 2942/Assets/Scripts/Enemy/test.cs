968f4b56bf98d0bd5bee94024f8e5fcef772bec5
sebastian gomez <sebbastian,gc@gmail.com>
2021-06-20 13:27:27 -0300

    Enemy basic  interactions

---
+++ TP11 - 2942/Assets/Scripts/Enemy/test.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+   
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        
+
+       
+        
+        if (other.transform.GetComponent<IHittable>() == null) return;
+        other.transform.GetComponent<IHittable>().Damage();
+        Debug.Log("entraste papu ");
+    }
+}
